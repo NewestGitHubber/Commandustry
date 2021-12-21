80b97b923f2743bc749d5b23f75853603ead3da6
SMOLKEYS <jiroshimonis@gmail.com>
2021-12-21 10:24:48 +0800

    jabaskipt

--- scripts/units/v3.js
+++ scripts/units/v3.js
@@ -1,4 +1,5 @@
-const weapons = require("units/weapons")
+const weapons = require("units/weapons");
 
 const v3 = extend(UnitType, "v3", {}); //fields are defined in its hjson file
+v3.constructor = () => extend(MechUnit, {});
 v3.weapons.add(weapons.v3Weapon);
