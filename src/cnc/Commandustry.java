4053d35a41ddfe5b4d8f4d51d2c09c0c61572090
jiroshimonis@gmail.com <jiroshimonis@gmail.com>
2022-03-28 18:30:59 +0800

    f

---
+++ src/cnc/Commandustry.java
@@ -0,0 +1,23 @@
+package cnc;
+
+import arc.*;
+import arc.util.*;
+import mindustry.*;
+import mindustry.content.*;
+import mindustry.game.EventType.*;
+import mindustry.gen.*;
+import mindustry.mod.*;
+import mindustry.ui.dialogs.*;
+
+public class Commandustry extends Mod{
+
+    public Commandustry(){
+        
+    }
+
+    @Override
+    public void loadContent(){
+        
+    }
+
+}
